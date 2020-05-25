179019771118a86c465e489e07827c19a91a286d
Diego <luquesdiego@outlook.com>
2020-05-25 13:52:58 -0400

    Listando produtos

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -21,6 +21,10 @@ export class ProductService {
     });
   }
 
+  read(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl)
+  }
+
   create(product: Product): Observable<Product> {
     return this.http.post<Product>(this.baseUrl, product)
   }
